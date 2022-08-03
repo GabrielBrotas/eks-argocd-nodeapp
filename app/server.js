eae5b1a82b777cd4aabe5f764dda343e3dd73367
Gabriel Brotas <gabriel.brotas@wexinc.com>
2022-08-03 13:56:38 -0300

    templates

---
+++ app/server.js
@@ -0,0 +1,13 @@
+const app = require("express")()
+const { hostname } = require("os")
+
+app.get('/', (req, res) => {
+    console.log("got here")
+    return res.status(200).json({ success: true, metadata: hostname() })
+})
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+    console.log(`App runnning on port ${PORT}`)
+})
