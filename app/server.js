904c78b09a3e552e98b3d0da837addcd310438d5
Gabriel Brotas <gabriel.brotas@wexinc.com>
2022-08-03 14:42:22 -0300

    new version

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     return res.status(200).json({ 
         success: true, 
         metadata: hostname(),
-        version: "1.1.0"
+        version: "1.2.0"
     })
 })
 
